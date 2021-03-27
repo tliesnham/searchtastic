0bcab84e5a2fac497598bf6bbb8e8d17033f7cb6
Taylor Liesnham <taylorliesnham@Taylors-MacBook-Air-10.local>
2021-03-27 11:46:11 +0000

    refactored code and updated test data

--- searchtastic/app.py
+++ searchtastic/app.py
@@ -1,12 +1,13 @@
 import json
 
+
 class Searchtastic:
     def search(keyword):
         with open("test_keyword_data.json", "r") as file:
             search_data = json.loads(file.read())
         keyword = keyword.lower()  # prevents case sensitivity
         results = []
-        
+
         for k, v in search_data.items():
             if keyword in k:
                 v.append(k)
